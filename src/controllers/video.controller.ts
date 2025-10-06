60d5a9ab4066e6efb8de443af3e05e05f637dcfc
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-10-06 16:57:43 -0300

    remove importacao desnecessaria

--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { VideoService } from "../services/video.service";
 import { z } from "zod";
 import { CustomError } from "../types/CustomError";
